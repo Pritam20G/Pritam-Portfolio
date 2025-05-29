b0253bcccab8b214e266f5a907fc0cf5e3152f38
Pritam20G <giripritam47@gmail.com>
2025-05-29 12:29:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', '&amp; Graphic Designer.'],
+    typeSpeed: 100,
+});
+
+
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        const targetPosition = targetElement.offsetTop;
+
+        window.scrollTo({
+            top: targetPosition - document.querySelector('nav').offsetHeight,
+            behavior: 'smooth'
+        })
+    })
+});
